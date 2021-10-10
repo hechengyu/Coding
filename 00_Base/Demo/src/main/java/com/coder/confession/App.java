83818a95b38db9f258f2000baa647c48d1d888b9
HeChengYu <hcy_email@163.com>
2021-10-10 20:54:10 +0800

    初始化
    Signed-off-by: HeChengYu <hcy_email@163.com>

---
+++ 00_Base/Demo/src/main/java/com/coder/confession/App.java
@@ -0,0 +1,13 @@
+package com.coder.confession;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @since 2021/9/16 20:53
+ */
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.info("App...");
+    }
+}
---
+++ 00_Base/Demo/src/test/java/com/coder/confession/AppTest.java
@@ -0,0 +1,13 @@
+package com.coder.confession;
+
+import org.junit.Test;
+
+/**
+ * @since 2021/9/16 20:54
+ */
+public class AppTest {
+    @Test
+    public void test() {
+        System.out.println("App Test");
+    }
+}
